f6a6ee3c135893bdd790545d997d61422522095c
trougnouf <trougnouf@gmail.com>
2018-05-20 13:15:14 +0200

    update grapher (x-axis has options)

--- grapher.py
+++ grapher.py
@@ -1,27 +1,36 @@
+# python grapher.py [--logdir <logdir>] [--x-axis time|gen]
+
 import sys
 import os
 import re
 import json
 import matplotlib.pyplot as plt
 from random import randint
-if len(sys.argv) <= 1:
-    logdir = "log"
+
+if '--logdir' in sys.argv:
+	logdir = sys.argv[sys.argv.index('--logdir')+1]
 else:
-    logdir = sys.argv[1]
+	logdir = 'log'
+if '--x-axis' in sys.argv:
+	(xaxis_name, xaxis_i) = {
+		'time': ('time (s)', 1),
+		'gen': ('# of generations', 0)
+	}[sys.argv[sys.argv.index('--x-axis')+1]]
+else:
+	(xaxis_name, xaxis_i) = ('time (s)', 1)
 
 filelist = os.listdir(logdir)
 
 #ver = re.findall("[0-9]+\.[0-9]+",filelist[0])[0]
 
-def get_models_time_bscore(logdir):
-
+def get_and_plot_models_xaxis_bscore(logdir):
     models={}
-    def get_time_bscore(fn):
-        time_bscore={}
+    def get_xaxis_bscore(fn):
+        xaxis_bscore={}
         with open(logdir+"/"+fn) as f:
             for line in json.load(f):
-                time_bscore[line[1]] = line[4]
-        return time_bscore
+                xaxis_bscore[line[xaxis_i]] = line[4]
+        return xaxis_bscore
     fig = None
     for fn in filelist:
         if "hist" not in fn:
@@ -32,15 +41,15 @@ def get_models_time_bscore(logdir):
             models[model] = {}
             colors=[0x00ffff, 0xff00ff, 0xffff00, 0x0000ff, 0x00ff00, 0xff0000]
             if fig is not None:
+                plt.legend()
                 plt.show()
             fig = plt.figure()
             figstuff = fig.add_subplot(111)
-            plt.legend(loc='upper left')
-            plt.xlabel("time (s)")
+            plt.xlabel(xaxis_name)
             plt.ylabel("score")
             plt.title(model)
 
-        time_bscore = get_time_bscore(fn)
+        xaxis_bscore = get_xaxis_bscore(fn)
         try:
             color = colors.pop()
         except IndexError:
@@ -52,14 +61,10 @@ def get_models_time_bscore(logdir):
             color='0'+color
         color='#'+color
         print(algo)
-        models[model][algo] = {'fn':fn, 'algo':algo, 'perf':time_bscore, 'color':color}
-        figstuff.scatter(time_bscore.keys(), time_bscore.values(), c=color, label=algo)
+        models[model][algo] = {'fn':fn, 'algo':algo, 'perf':xaxis_bscore, 'color':color}
+        figstuff.scatter(xaxis_bscore.keys(), xaxis_bscore.values(), c=color, label=algo)
+    plt.legend()
     plt.show()
     return models
-models = get_models_time_bscore(logdir)
+models = get_and_plot_models_xaxis_bscore(logdir)
 
-
-
-
-
-print(models)
